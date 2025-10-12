98e029c2719460bde86b87714767890b1ff61727
Richard Palacios <apalaciosg91@gmail.com>
2025-10-12 07:55:05 -0500

    feat(datatypes): add datatypes on c#

---
+++ S2/HelloApp/01-Bases/DataTypes.cs
@@ -0,0 +1,12 @@
+partial class Program
+{
+  static void DataTypes()
+  {
+    int integer = 42;
+    double decimalNumber = 3.1416;
+    bool isTrue = true;
+    char character = 'C';
+    string text = "Hola C#";
+    Console.WriteLine($"Entero: {integer}, Decimal: {decimalNumber}, Booleano: {isTrue}, Caracter: {character}, Texto: {text}");
+  }
+}
