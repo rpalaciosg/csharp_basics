2ace65adcf93cce911de5a0f1da84e559142f941
Richard Palacios <apalaciosg91@gmail.com>
2025-10-12 07:54:38 -0500

    feat(variables): add variables

---
+++ S2/HelloApp/01-Bases/Variables.cs
@@ -0,0 +1,12 @@
+partial class Program
+{
+  static void Variables()
+  {
+    int edad = 25;
+
+    string nombre = "Juan";
+    bool esEstudiante = true;
+
+    Console.WriteLine($"Nombre: {nombre} tiene una edad {edad} es estudiante {esEstudiante}");
+  }
+}
