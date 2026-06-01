107ad87e492e04592f5f17cc7d8c10082ebcb397
Richard Palacios <apalaciosg91@gmail.com>
2026-05-31 23:14:18 -0500

    feat: config importacion estatica
    - permite poder usar el `WriteLine` y `ReadLine` sin `Console.` antes.

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # Csharp solid fundations
 
 Reinforcing basics C# concepts
+
+## Configuracion importacion Statica
+
+Para qeu podamos usar solo `WriteLine` en lugar de `System.Console.WriteLine` aplicamos esta configuracion
+en el archivo del proyecto `.csproj` y agregamos lo siguiente:
+
+```xml
+  <ItemGroup Label="Simplificar el uso de la consola">
+    <Using Include="System.Console" Static="true"/>
+  </ItemGroup>
+```
--- S2/HelloApp/01-Bases/Generics.cs
+++ S2/HelloApp/01-Bases/Generics.cs
@@ -11,16 +11,16 @@ partial class Program
     string[] names = {"Juan", "Luis", "Barak", "Evan"};
     int[] numbers = {1,2,3};
     // usando metodos repetidos normales sin genericos
-    Console.WriteLine($"Tamanio del arreglo numerico: { GetIntArrayLength(numbers) } ");
-    Console.WriteLine($"Tamanio del arreglo nombres: { GetStringArrayLength(names) } ");
+    WriteLine($"Tamanio del arreglo numerico: { GetIntArrayLength(numbers) } ");
+    WriteLine($"Tamanio del arreglo nombres: { GetStringArrayLength(names) } ");
 
     // Usando un metodo generico
-    Console.WriteLine("\n usando un metodo generico:");
-    Console.WriteLine($"Tamanio del arreglo nombres con un metodo generico: { GetArrayLength(names) } ");
-    Console.WriteLine($"Tamanio del arreglo numerico con un metodo generico: {GetArrayLength(numbers)}");
+    WriteLine("\n usando un metodo generico:");
+    WriteLine($"Tamanio del arreglo nombres con un metodo generico: { GetArrayLength(names) } ");
+    WriteLine($"Tamanio del arreglo numerico con un metodo generico: {GetArrayLength(numbers)}");
 
     // Usando clase Generica
-    Console.WriteLine("\n usando un Clase generica:");
+    WriteLine("\n usando un Clase generica:");
     Box<int> numberBox = new Box<int> { Content=50 };
     Box<string> stringBox = new Box<string> { Content="Ahora soy texto." };
     numberBox.Show();
@@ -56,6 +56,6 @@ class Box<T>
   public T? Content  {get; set;}
   public void Show()
   {
-    Console.WriteLine($"Contendio: {Content}");
+    WriteLine($"Contendio: {Content}");
   }
 }
