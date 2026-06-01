0e4e5db0363ccde4c718be3fdadb0af89cb4c0b9
Richard Palacios <apalaciosg91@gmail.com>
2026-05-31 19:46:58 -0500

    feat: Clases genericas

--- S2/HelloApp/01-Bases/Generics.cs
+++ S2/HelloApp/01-Bases/Generics.cs
@@ -19,6 +19,13 @@ partial class Program
     Console.WriteLine($"Tamanio del arreglo nombres con un metodo generico: { GetArrayLength(names) } ");
     Console.WriteLine($"Tamanio del arreglo numerico con un metodo generico: {GetArrayLength(numbers)}");
 
+    // Usando clase Generica
+    Console.WriteLine("\n usando un Clase generica:");
+    Box<int> numberBox = new Box<int> { Content=50 };
+    Box<string> stringBox = new Box<string> { Content="Ahora soy texto." };
+    numberBox.Show();
+    stringBox.Show();
+
   }
 
   // metodos sin el uso de genericos
@@ -32,10 +39,23 @@ partial class Program
     return array.Length;
   }
 
+  // Metodo Generico
   // vamos a crear un `Metodo Generico` que me devuelva el tamanio de cada arreglo
   // sin importar el tipo
+  // estos metodos genericos nos ayudan a no duplicar codigo
   static int GetArrayLength<T>(T[] array)
   {
     return array.Length;
+  }
+}
+
+// Clase generica
+// nos permiten trabajar con diferentes tipos de datos
+class Box<T>
+{
+  public T? Content  {get; set;}
+  public void Show()
+  {
+    Console.WriteLine($"Contendio: {Content}");
   }
 }
