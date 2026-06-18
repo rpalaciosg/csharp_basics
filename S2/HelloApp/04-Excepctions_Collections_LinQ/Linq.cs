f0ba6b03e440642dac568da7992d2fa2298aafbd
Richard Palacios <rpalacios@mutazuay.com>
2026-06-18 17:01:03 -0500

    linq: consultas simples y transformacion de la coleccion

--- S2/HelloApp/04-Excepctions_Collections_LinQ/Linq.cs
+++ S2/HelloApp/04-Excepctions_Collections_LinQ/Linq.cs
@@ -28,7 +28,51 @@ partial class Program
 
     foreach (var number in evenNumbersMethod)
     {
-      WriteLine(number);
+      // WriteLine(number);
+    }
+    
+    /*
+    * Consultas simples
+    */
+    List<MarvelCharacter> characters = new List<MarvelCharacter>
+    {
+      new MarvelCharacter { Name = "Peter Parker", Alias = "Spider-Man", Team = "Avengers" },
+      new MarvelCharacter { Name = "Tony Stark", Alias = "Iron Man", Team = "Avengers" },
+      new MarvelCharacter { Name = "Steve Rogers", Alias = "Captain America", Team = "Avengers" },
+      new MarvelCharacter { Name = "Natasha Romanoff", Alias = "Black Widow", Team = "Avengers" },
+      new MarvelCharacter { Name = "T'Challa", Alias = "Black Panther", Team = "Wakanda" },
+      new MarvelCharacter { Name = "Stephen Strange", Alias = "Doctor Strange", Team = "Defenders" }
+     };
+    
+    // personajes del team avengers
+    // WriteLine(">> Personasjes que pertenecen a los Avengers <<");
+    var avengersQuery = from c in characters
+                        where c.Team == "Avengers"
+                        select $"{c.Alias} {c.Name}";
+    
+    var avengersMethod = characters.Where(c => c.Team == "Avengers");
+    // foreach (var character in avengersMethod)
+    // {
+    // //  WriteLine($"{character.Alias} {character.Name}"); 
+    // }
+    
+    // podemos transformar la coleccion de datos con nuestro select
+    //que todos los nombres esten en mayusculas
+    var uppercaseNamesQuery = from c in characters
+                              select c.Name?.ToUpper();
+    
+    var uppercaseNamesMethod = characters.Select(c => c .Name?.ToUpper());
+    WriteLine(">> Nombres de Personasjes en mayusculas <<");
+    foreach (var characterName in uppercaseNamesMethod)
+    {
+      WriteLine($"{characterName}"); 
     }
   }
+}
+
+class MarvelCharacter
+{
+    public string? Name { get; set; }
+    public string? Alias { get; set; }
+    public string? Team { get; set; }
 }
