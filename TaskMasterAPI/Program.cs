99f7029bf1c3e3c0c36c4a2ec59c956feb2d669f
Richard Palacios <apalaciosg91@gmail.com>
2026-08-16 14:47:58 -0500

    TaskMasterAPI: add scalar tool instead of Swagger

--- TaskMasterAPI/Program.cs
+++ TaskMasterAPI/Program.cs
@@ -1,3 +1,5 @@
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
@@ -9,6 +11,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference();
 }
 
 app.UseHttpsRedirection();
